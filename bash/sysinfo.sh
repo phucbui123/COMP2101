612246584e263db21f6e1edd186fae19e1f929c9
Phuc Bui <comp2101-02@zonzorp.net>
2022-10-05 05:42:25 -0400

    Bash 2 sysinfo.sh updated

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
-#Script Challenge
-#The system’s fully-qualified domain name (FQDN)
-hostname -f
-#The operating system name and version
-hostnamectl
-#Any IP addresses the machine has that are not on the 127 network
-hostname -I
-#The amount of space available in only the root filesystem
-df -h |head -1
-df -h | grep -w /
+#Script Challenge - BASH 2
+#Creat variables for my report data
+HostName=$(hostname)	#Variable for host name
+FQDN=$(hostname -f)	#Variable for fully qualified domain name
+Ip=$(ip route | tail -1 | awk '{print $9}')	#Variable for IP address
+SeperLine================	#Variable for Seperator line
+OperatingSys=$(hostnamectl | grep -w Operating | awk '{print $3" "$4" "$5}')	#Variable for operating system name and version
+AvailSpace=$(df -h | grep -w / | awk '{print $4}')	#Variable for free disk space
+#Blank line
+echo
+#Title
+echo Report for $HostName
+#Seperator line
+echo $SeperLine
+#Report FQDN
+echo FQDN\: $FQDN
+#Report Operating System name and version
+echo Operating System name and version\: $OperatingSys
+#Report IP address
+echo IP Address: $Ip
+#Report free disk space in human-friendly number
+echo Root Filesystem Free Space\: $AvailSpace
+#Seperator line
+echo $SeperLine
+
