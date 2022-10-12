b5506001bd28f1fd6c991b2be8e9225b8e3da282
Phuc Bui <comp2101-02@zonzorp.net>
2022-10-12 03:18:01 -0400

    Bash 3 updated

--- bash/bash3.sh
+++ bash/bash3.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+#test for lxd file and success test comment
+test -e ~/snap/lxd && echo "lxd software exists"
+#test for Apache2 file in the container and success comment
+lxc exec COMP2101-S22 test /etc/apache2 &&
+	echo "Apache2 in the container exists"
+#test for host name COMP2101-S22 in /etc/hosts
+grep -q COMP2101-S22 /etc/hosts && echo "COMP2101-S22 was added to /etc/hosts with current IP address"
 #retrieve the default web page from the container’s web service and notify the user of success or failure
 curl http://COMP2101-S22 &&
 	echo "Successfully retrieved" ||
