e504bd4e83a1ab7a3e6af05bdace707b8034cf07
phucbui123 <200498460@student.georgianc.on.ca>
2022-11-15 21:40:33 -0500

    Update lab3.ps1

--- powershell/lab3.ps1
+++ powershell/lab3.ps1
@@ -1,1 +1,5 @@
-﻿
+﻿Get-CimInstance -ClassName Win32_networkadapterconfiguration |
+Where-Object { 
+$_.ipenabled -eq "true"
+} | 
+ft -a Description, Index, IPAddress, IPSubnet, DNSDomain, DNSServersearchorder
